0f6616e61c00a9e8e1740ebb2a13a93c838709fe
Jono Sommers <jonosommers95@gmail.com>
2025-05-31 15:37:47 -0500

    created landing page and navbar.

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,19 @@
+import './LandingPage.css'
+import MainNavBar from '../NavBars/MainNavBar'
+
 function LandingPage() {
     return (
-        <h1>Dexly</h1>
+        <section className='landing-page-container'>
+            <div className='navbar'>
+                <MainNavBar />
+            </div>
+
+            <div className='header-section'>
+                <header>
+                    <h1>Dexly</h1>
+                </header>
+            </div>
+        </section>
     )
 }
 
---
+++ src/components/NavBars/MainNavBar.jsx
@@ -0,0 +1,11 @@
+function MainNavBar() {
+    return (
+        <section className='main-navbar'>
+            <header>
+                <h1>Hello</h1>
+            </header>
+        </section>
+    )
+}
+
+export default MainNavBar
