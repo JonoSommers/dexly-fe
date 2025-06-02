bed5316d3e5186d2db747f7c762264d185a83517
Jono Sommers <jonosommers95@gmail.com>
2025-06-02 14:20:14 -0500

    navbar is styled, added card outlines to fill in blank space, and act as outlines for favorites section after mvp.

--- src/components/NavBars/MainNavBar.jsx
+++ src/components/NavBars/MainNavBar.jsx
@@ -1,28 +1,28 @@
 import { NavLink } from "react-router-dom"
+import "./MainNavBar.css"
 
 function MainNavBar() {
     return (
         <nav className="navbar">
             <div className="navbar-left">
-                <a href="/" className="logo">Dexly</a>
+                <NavLink to="/" className="logo">Dexly</NavLink>
             </div>
 
-            <div className="navbar-center">
-                <ul className="nav-links">
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/cards">Cards</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/login">Login</NavLink>
-                    </li>
-                </ul>
+            <div className="favorites-bar">
+                {[...Array(5)].map((_, i) => (
+                    <div key={i} className="empty-card-slot"></div>
+                ))}
             </div>
 
             <div className="navbar-right">
-                <input className="nav-search-bar"
+                <ul className="nav-links">
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/cards">Cards</NavLink></li>
+                    <li><NavLink to="/login">Login</NavLink></li>
+                </ul>
+
+                <input
+                    className="nav-search-bar"
                     type="text"
                     placeholder="Search..."
                 />
