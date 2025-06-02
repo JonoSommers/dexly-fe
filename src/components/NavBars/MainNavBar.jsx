e6efd1dc87afe353aa720f2b999543f5622a97c3
Jono Sommers <jonosommers95@gmail.com>
2025-06-02 13:16:44 -0500

    landing page and navbar foundation build out. next step: styling, following setp: build in functionality and state.

--- src/components/NavBars/MainNavBar.jsx
+++ src/components/NavBars/MainNavBar.jsx
@@ -1,10 +1,33 @@
+import { NavLink } from "react-router-dom"
+
 function MainNavBar() {
     return (
-        <section className='main-navbar'>
-            <header>
-                <h1>Nav Bar</h1>
-            </header>
-        </section>
+        <nav className="navbar">
+            <div className="navbar-left">
+                <a href="/" className="logo">Dexly</a>
+            </div>
+
+            <div className="navbar-center">
+                <ul className="nav-links">
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/cards">Cards</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/login">Login</NavLink>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="navbar-right">
+                <input className="nav-search-bar"
+                    type="text"
+                    placeholder="Search..."
+                />
+            </div>
+        </nav>
     )
 }
 
