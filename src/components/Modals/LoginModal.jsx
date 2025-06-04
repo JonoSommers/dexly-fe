00e9a7834ed2570b7f27316510df2ddac1bcec44
Jono Sommers <jonosommers95@gmail.com>
2025-06-04 15:30:31 -0500

    User data is not retrieved and stored in state after logging in.

--- src/components/Modals/LoginModal.jsx
+++ src/components/Modals/LoginModal.jsx
@@ -1,6 +1,18 @@
+import useUserStore from '../../store/useUserStore'
+import { useState } from 'react'
 import "./LoginModal.css"
 
 function LoginModal() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const error = useUserStore(state => state.error)
+    const loginUser = useUserStore(state => state.loginUser)
+
+    function handleLogin(event) {
+        event.preventDefault()
+        loginUser(username, password)
+    }
+
     return (
         <section className="login-container">
             <section className="login-form">
@@ -13,6 +25,8 @@ function LoginModal() {
                     className="login-username"
                     type="text"
                     placeholder="Username..."
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
                 />
 
                 <h4>Password</h4>
@@ -20,8 +34,11 @@ function LoginModal() {
                     className="login-password"
                     type="password"
                     placeholder="Password..."
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                 />
-                <button className="login-button">Login</button>
+                <button onClick={handleLogin} className="login-button">Login</button>
+                {error && <p className="error-message">{error}</p>}
             </section>
         </section>
     )
--- src/store/useUserStore.js
+++ src/store/useUserStore.js
@@ -1,19 +1,34 @@
 import { create } from 'zustand'
 
-const useUserStore = create((set) ({
+const useUserStore = create((set) => ({
+    error: null,
+    setError: (errorMsg) => set({ error: errorMsg }),
     user: null,
     binders: [],
     setUser: (userData) => set({ user: userData}),
     setBinders: (binders) => set({ binders }),
     logout: () => set({ user: null, binders: [] }),
-    fetchUser: (id) => {
-        fetch(`/api/v1/users/${id}`, {
+    loginUser: (username, password) => {
+        fetch('http://localhost:3000/api/v1/login', {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
-            }
+            },
+            body: JSON.stringify({ username, password })
         })
-            .then(response => response.json())
-            .then(data => set({ user: data.data }))
-            .catch(error => console.log('error message: ', error.message))
-    } 
+            .then(async response => {
+                const data = await response.json()
+                if (!response.ok) {
+                    throw new Error(data.error || 'Failed to fetch all users')
+                }
+                console.log(data)
+                set({ user: data.data, binders: data.data.attributes.binders || null, error: null })
+            })
+            .catch(error => {
+                console.log('error message: ', error.message)
+                set({ error: error.message })
+            })
+    }
 }))
+
+export default useUserStore
